d1226cbf89839d48ee2c342c7e01c55fd0349908
vanyakozub <59049483+vanyakozub@users.noreply.github.com>
2021-01-25 10:56:18 +0300

    Changed algorithm to one loop

--- Task2/src/main/java/ListCleaner.java
+++ Task2/src/main/java/ListCleaner.java
@@ -1,51 +1,45 @@
 import java.util.*;
+
 public class ListCleaner {
     public static void removeSame(List<Integer> list) {
         List<Integer> toRemove = new ArrayList<Integer>();
-        List<Integer> repeat = new ArrayList<Integer>();
+        HashMap<Integer, List<Integer>> repeats = new HashMap<Integer, List<Integer>>();
         Iterator<Integer> it1 = list.iterator();
         while (it1.hasNext()) {
             Integer val = it1.next();
-            Iterator<Integer> it2 = list.iterator();
-            while (it2.hasNext()) {
-                Integer curValue = it2.next();
-                if(val.equals(curValue)) {
-                    repeat.add(curValue);
-                }
+            if (!repeats.containsKey(val)) {
+                repeats.put(val, new ArrayList<Integer>());
             }
-            if(repeat.size() >= 3) {
-                toRemove.addAll(repeat);
+            repeats.get(val).add(val);
+            if (repeats.get(val).size() >= 3) {
+                toRemove.addAll(repeats.get(val));
             }
-            repeat.clear();
         }
         list.removeAll(toRemove);
     }
+
     public static void removeSameInRow(List<Integer> list) {
         List<Integer> toRemove = new ArrayList<Integer>();
-        List<Integer> repeat = new ArrayList<Integer>();
+        List<Integer> subRow = new ArrayList<Integer>();
+        HashMap<Integer, List<Integer>> repeats = new HashMap<Integer, List<Integer>>();
         Iterator<Integer> it1 = list.iterator();
         while (it1.hasNext()) {
-            boolean needToRemove = false;
             Integer val = it1.next();
-            int amount = 0;
-            Iterator<Integer> it2 = list.iterator();
-            while (it2.hasNext()) {
-                Integer curValue = it2.next();
-                if(val.equals(curValue)) {
-                    repeat.add(curValue);
-                    amount++;
-                    if(amount >=3) {
-                        needToRemove = true;
-                    }
-                }
-                else {
-                    amount = 0;
-                }
+            if(subRow.size() !=3){
+                subRow.add(val);
             }
-            if(needToRemove) {
-                toRemove.addAll(repeat);
+            else {
+                subRow.set(0,subRow.get(1));
+                subRow.set(1,subRow.get(2));
+                subRow.set(2, val);
             }
-            repeat.clear();
+            if (!repeats.containsKey(val)) {
+                repeats.put(val, new ArrayList<Integer>());
+            }
+            repeats.get(val).add(val);
+            if (subRow.size() == 3 &&subRow.get(0).equals(subRow.get(1)) && subRow.get(0).equals(subRow.get(2))) {
+                toRemove.addAll(repeats.get(val));
+            }
         }
         list.removeAll(toRemove);
     }
